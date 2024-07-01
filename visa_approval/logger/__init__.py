c91443e0f07b634ee99784f3953a21904565ae75
arshil <arshilsingh711@gmail.com>
2024-07-01 19:20:32 +0530

    done with logging

---
+++ visa_approval/logger/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import os
+from from_root import from_root
+
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%m_%S')}"
+
+log_dir = 'logs'
+
+log_path = os.path.join(from_root(), log_dir, LOG_FILE)
+
+os.makedirs(log_dir)
+
+logging.basicConfig(filename=log_path,
+                    format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
+                     level=logging.DEBUG)
